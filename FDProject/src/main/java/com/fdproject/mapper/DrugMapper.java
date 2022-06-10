81239078e674b9440fbd6adcb93e5964155c45d5
wonjaekang <kang471330@gmail.com>
2022-06-10 16:31:39 +0900

    modify

--- FDProject/src/main/java/com/fdproject/mapper/DrugMapper.java
+++ FDProject/src/main/java/com/fdproject/mapper/DrugMapper.java
@@ -1,5 +1,20 @@
 package com.fdproject.mapper;
 
+import com.fdproject.domain.DrugDTO;
+import com.fdproject.domain.UserDrugDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface DrugMapper {
+
+    List<DrugDTO> drugList(DrugDTO drugDTO);
+
+    List<DrugDTO> riskDrugList(DrugDTO drugDTO);
+
+    List<String> selectKeyword();
+
+    int selectDrugTotalCount(DrugDTO drugDTO);
 
 }
