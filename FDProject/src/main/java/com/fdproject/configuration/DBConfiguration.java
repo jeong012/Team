683743848eb088309b7eeb2118979dc294d76f11
jeong012 <ldj10235@gmail.com>
2022-06-15 11:06:47 +0900

    오류 수정
    xml 파일 mapper 오류

--- FDProject/src/main/java/com/fdproject/configuration/DBConfiguration.java
+++ FDProject/src/main/java/com/fdproject/configuration/DBConfiguration.java
@@ -47,7 +47,7 @@ public class DBConfiguration {
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
         factoryBean.setDataSource(dataSource());
         factoryBean.setMapperLocations(applicationContext.getResources("classpath:/mappers/**/*Mapper.xml"));
-        factoryBean.setTypeAliasesPackage("com.fdproject.domain");
+        factoryBean.setTypeAliasesPackage("com.fdproject.*");
         factoryBean.setConfiguration(mybatisConfig());
         return factoryBean.getObject();
     }
