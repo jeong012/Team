5ee6fac3c7bb1fe4ce8adc9f62739ecaa8a09b3c
sunini9832 <sunini9832@gmail.com>
2022-06-20 16:18:04 +0900

    disease list

--- FDProject/src/main/java/com/fdproject/service/DiseaseService.java
+++ FDProject/src/main/java/com/fdproject/service/DiseaseService.java
@@ -1,5 +1,13 @@
 package com.fdproject.service;
 
+import java.util.List;
+
+import com.fdproject.domain.DiseaseDTO;
+import com.fdproject.domain.DrugDTO;
+import com.fdproject.domain.UserDiseaseDTO;
+
 public interface DiseaseService {
+
+	List<DiseaseDTO> getDiseaseList(String id, DiseaseDTO params);
 
 }
